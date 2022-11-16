83d4f8f8e1747ed471a33d49ba24a74a0827f11f
Rogderina <78894739+rogderina@users.noreply.github.com>
2022-11-16 15:11:11 +0700

    Update promet_trst.py

--- all/chek_stream/playable-streams/promet_trst.py
+++ all/chek_stream/playable-streams/promet_trst.py
@@ -4,7 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import logging
 
-response = requests.get("http://93.191.11.247:9090/api/v1/query?query=streammanager_transcoder_inputfps",
+response = requests.get("http:",
     headers={
         "Authorization": "Basic YW5hbHl6ZTppZGRxZGlka2Zh"
     }
@@ -13,12 +13,12 @@ parametrs = response.json()
 eventcode_arr = []
 for ar in parametrs['data']['result']:
     try:
-        response_code = requests.post("https://app-3.facecast.net/api/v1/get_event",
-                                      data={"uid": "14745", "api_key": "N42tZO89OP8X36jwjm711vlauReYpRL2",
+        response_code = requests.post("https:",
+                                      data={"uid": "14745", "api_key": "",
                                             "event_id": ar['metric']['eventid']})
         param_code = response_code.json()
         driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.get("https://facecast.net/w/" + param_code['code'] + "?muted=1&autorun=1")
+        driver.get("https:" + param_code['code'] + "?muted=1&autorun=1")
         time.sleep(6)
         dictionary = {
             'level': "info",
@@ -35,18 +35,18 @@ for ar in parametrs['data']['result']:
 
     except:
         logging.warning('Трансляция с доступом')
-        response_code_1 = requests.post("https://app-3.facecast.net/api/v1/get_event",
+        response_code_1 = requests.post("https:",
                                       data={"uid": "14745", "api_key": "N42tZO89OP8X36jwjm711vlauReYpRL2",
                                             "event_id": ar['metric']['eventid']})
         param_code_1 = response_code_1.json()
         channel_id_code = param_code_1['channel_id']
         time.sleep(6)
-        admin_key = requests.get(f"http://app-3.facecast.net:8080/live?ajaj=1&mode=get_current_event&channel_id={channel_id_code}&uid=14745&api_key=N42tZO89OP8X36jwjm711vlauReYpRL2")
+        admin_key = requests.get("http:)
         param_code_key = admin_key.json()
         driver.quit()
         param_code_key_a = param_code_key['admin_key']
         driver_key = webdriver.Chrome(ChromeDriverManager().install())
-        driver_key.get("https://facecast.net/w/" + param_code_1['code'] + "?key=" + param_code_key_a + "&muted=1&autorun=1")
+        driver_key.get("https:" + param_code_1['code'] + "?key=" + param_code_key_a + "&muted=1&autorun=1")
         time.sleep(3)
         dictionary1 = {
                 'level': "info",
